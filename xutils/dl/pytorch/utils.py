cb8a3ea41ebaef5b1071f62b5c6554057affef4b
Michael Bernstein <mrb1778@hotmail.com>
2024-03-25 17:26:52 -0700

    add print_device

--- xutils/dl/pytorch/utils.py
+++ xutils/dl/pytorch/utils.py
@@ -29,8 +29,23 @@ def num_gpus():
     return torch.cuda.device_count()
 
 
+def print_device():
+    # setting device on GPU if available, else CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    print()
+
+    # Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
+
+
 def free_memory():
     torch.cuda.empty_cache()
+    pyu.free_memory()
 
 
 def model_summary(model, count_params=True) -> str:
